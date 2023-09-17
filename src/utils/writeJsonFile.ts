b20d0d14d03477d17b3ca65b0e6cd695d0487693
JeffersonAlves7 <ilj031420@hotmail.com>
2023-09-17 15:31:41 -0300

    style: format writeJsonFile.ts with prettier

--- src/utils/writeJsonFile.ts
+++ src/utils/writeJsonFile.ts
@@ -1,5 +1,5 @@
-import fs from "fs"
+import fs from "fs";
 
-export default function writeJsonFile(fileName: string, content: any){
+export default function writeJsonFile(fileName: string, content: any) {
   fs.writeFileSync("temp/" + fileName, JSON.stringify(content, null, 2));
 }
